923276790179b51915688c8e4b3e1a23f2348ec2
TimmieQi <yuanchongxin@stu.scu.edu.cn>
2024-07-18 10:05:54 +0800

    Initial commit

---
+++ src/main/java/com/example/auctionplatform/AuctionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionPlatformApplication.class, args);
+	}
+
+}
