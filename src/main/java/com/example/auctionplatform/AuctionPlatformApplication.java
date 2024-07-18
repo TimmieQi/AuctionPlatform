07f7a5065bff25914144c26e0eb589262e491627
TimmieQi <yuanchongxin@stu.scu.edu.cn>
2024-07-18 10:09:59 +0800

    初始状态

--- src/main/java/com/example/auctionplatform/AuctionPlatformApplication.java
+++ src/main/java/com/example/auctionplatform/AuctionPlatformApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuctionPlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuctionPlatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionPlatformApplication.class, args);
+    }
 
 }
